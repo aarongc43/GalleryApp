d8396423864b46ce79c358086c0f2580ec0447fc
aarongc43 <aarongc43@gmail.com>
2023-11-15 17:15:49 -0600

    modified:   GalleryApp/ArtistProfile.tsx

--- GalleryApp/ArtistProfile.tsx
+++ GalleryApp/ArtistProfile.tsx
@@ -8,33 +8,43 @@ import storage from '@react-native-firebase/storage';
   4. exhibition name 'exibitionName'
 */
 
-const ARTISTS_PATH = 'gs://art-gallery-app-f773a.appspot.com';
+interface Artist {
+  name: string;
+  'exhibition name': string;
+  location: string;
+  pieces: {
+    imageName: string;
+  }[];
+}
+
 const ARTISTS_JSON_PATH = 'gs://art-gallery-app-f773a.appspot.com/artist.json';
 
 export const fetchArtistFlatlistProfile = async (artistName: string) => {
   // fetch JSON object for artistst
-  const artistsJsonUrl = await storage().ref(ARTISTS_JSON_PATH).getDownloadURL();
+  const artistsJsonUrl = await storage()
+    .ref(ARTISTS_JSON_PATH)
+    .getDownloadURL();
 
-  // fetch JSON contents from the URL 
-  const response = await fetch(artistJsonUrl);
+  // fetch JSON contents from the URL
+  const response = await fetch(artistsJsonUrl);
   const artistsData = await response.json();
 
   // find artists data
-  const artistData = artistsData.artists.find(artist => artist.name === artistName);
-  if (!artistsData) {
-    throw new Error('Artists ${artistName} not found');
+  const artistData = artistsData.artists.find((artist: Artist) => {
+    return artist.name === artistName;
+  });
+  if (!artistData) {
+    throw new Error(`Artists ${artistName} not found`);
   }
 
   // fetch URLs for artist data from firebase
-  const exhibitionImage = await storage().ref('${artistData.pieces[0].imageName}').getDownloadURL();
-  const artistBio = await storage.ref(artistData.bio).getDownloadURL();
-  const artistExhibitName = artistData.exhibitionName();
-  const exhibitionLocation = artistData.location();
+  const exhibitionImage = await storage()
+    .ref('${artistData.pieces[0].imageName}')
+    .getDownloadURL();
 
   return {
     exhibitionImage,
-    artistExhibitName,
-    artistBio,
-    exhibitionLocation,
+    artistExhibitName: artistData['exhibition name'],
+    exhibitionLocation: artistData.location,
   };
 };
