43f4257ad0264bf54911baa374ddce02ea366737
aarongc43 <aarongc43@gmail.com>
2023-11-15 15:29:36 -0600

    modified:   ArtistProfile.tsx 	new file:   artists.json

--- GalleryApp/ArtistProfile.tsx
+++ GalleryApp/ArtistProfile.tsx
@@ -1,22 +1,40 @@
-import React from 'react';
-import {View, Text, Image} from 'react-native';
+import storage from '@react-native-firebase/storage';
 
-const ArtPiece = ({title, imageURL, description}) => (
-  <View>
-    <Image source={{uri: imageURL}} style={{width: 100, height: 100}} />
-    <Text>{title}</Title>
-    <Text>{description}</Text>
-  </View>
-)
+/*
+  A component for the home page flat list that will display
+  1. an art piece 'exibitionImage'
+  2. artist name 'artistName'
+  3. location hard set location
+  4. exhibition name 'exibitionName'
+*/
 
-const ArtistProfile = ({artistName, bio, imageURL, exhibition, artPieces}) => {
-  return (
-    <View>
-      <Text>{artistName}</Text>
-      <Image source={{uri: imageURL}} style={{width: 100, height: 100}} />
-      <Text>{bio}</Text>
-    </View>
-  );
+const ARTISTS_PATH = 'gs://art-gallery-app-f773a.appspot.com';
+const ARTISTS_JSON_PATH = 'gs://art-gallery-app-f773a.appspot.com/artist.json';
+
+export const fetchArtistFlatlistProfile = async (artistName: string) => {
+  // fetch JSON object for artistst
+  const artistsJsonUrl = await storage().ref(ARTISTS_JSON_PATH).getDownloadURL();
+
+  // fetch JSON contents from the URL 
+  const response = await fetch(artistJsonUrl);
+  const artistsData = await response.json();
+
+  // find artists data
+  const artistData = artistsData.artists.find(artist => artist.name === artistName);
+  if (!artistsData) {
+    throw new Error('Artists ${artistName} not found');
+  }
+
+  // fetch URLs for artist data from firebase
+  const exhibitionImage = await storage().ref('${artistData.pieces[0].imageName}').getDownloadURL();
+  const artistBio = await storage.ref(artistData.bio).getDownloadURL();
+  const artistExhibitName = artistData.exhibitionName();
+  const exhibitionLocation = artistData.location();
+
+  return {
+    exhibitionImage,
+    artistExhibitName,
+    artistBio,
+    exhibitionLocation,
+  };
 };
-
-export default ArtistProfile;
